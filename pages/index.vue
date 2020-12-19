d70b8f17b9fd8872e08e5f5cfb439fd2477c7b1c
Wouter Comello <woutxd@hotmail.com>
2020-12-19 22:45:38 +0100

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        portfolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+ <div>
+   <p>Test website</p>
+ </div>
 </template>
 
 <script>
